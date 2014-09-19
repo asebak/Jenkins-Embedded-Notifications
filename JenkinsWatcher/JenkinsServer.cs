389cd86c5570d287e2e9219a0d0a74a0f8f4b1d7
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-18 21:45:21 -0400

    added json.net and started making jenkins server c# api

---
+++ JenkinsWatcher/JenkinsServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JenkinsWatcher
+{
+    public class JenkinsServer
+    {
+        private Timer mTimer;
+        private DateTime mStopTime;
+        public void StartConnection()
+        {
+            this.mStopTime = DateTime.Now.AddMinutes(10);
+            this.mTimer = new Timer(WatchDogCallBack, null, 0, 5000);
+        }
+
+        private void WatchDogCallBack(object state)
+        {
+            if (DateTime.Now >= this.mStopTime)
+            {
+                this.mTimer.Dispose();
+                return;
+            }
+            // do your work.....
+        }
+    }
+}
--- JenkinsWatcher/MainWindow.xaml.cs
+++ JenkinsWatcher/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,19 @@ namespace JenkinsWatcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        private JenkinsServer mServer;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.JenkinsWatch.Click += JenkinsWatch_Click;
+
+        }
+
+        private void JenkinsWatch_Click(object sender, RoutedEventArgs e)
+        {
+            this.JenkinsWatch.IsEnabled = false;
+            this.mServer = new JenkinsServer();
         }
     }
 }
