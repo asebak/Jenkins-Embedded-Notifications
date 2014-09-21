32725a6534aab5e01817218ddb4273ebbfb2b327
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-21 19:55:56 -0400

    Converted to Maven Project and Added Jenkins Api Communication

--- JenkinsWatcherJava/src/com/jenkinswatch/pkg/JenkinsJobOverview.java
+++
@@ -1,7 +0,0 @@
-package com.jenkinswatch.pkg;
-
-/**
- * Created by asebak on 9/21/2014.
- */
-public class JenkinsJobOverview {
-}
--- JenkinsWatcherJava/src/com/jenkinswatch/pkg/Main.java
+++
@@ -1,9 +0,0 @@
-package com.jenkinswatch.pkg;
-
-public class Main {
-
-	public static void main(String[] args) {
-        JenkinsWatcherForm jenkinsWatcherForm = new JenkinsWatcherForm();
-	}
-
-}
