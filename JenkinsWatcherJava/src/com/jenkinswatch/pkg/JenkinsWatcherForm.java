4403c9efb6f6f3acffd253168eb6435397b31c24
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-21 17:00:28 -0400

    Updated Form controls

--- JenkinsWatcherJava/src/com/jenkinswatch/pkg/JenkinsWatcherForm.java
+++ JenkinsWatcherJava/src/com/jenkinswatch/pkg/JenkinsWatcherForm.java
@@ -6,12 +6,13 @@ import javax.swing.*;
  * Created by asebak on 9/21/2014.
  */
 public class JenkinsWatcherForm extends JFrame {
-    private JTextField textField1;
-    private JPanel rootPanel;
+    private JPanel jPanel;
+    private JTextField serverField;
+    private JButton refreshBtn;
 
     public JenkinsWatcherForm(){
         super("Jenkins Real-Time Build Monitor");
-        this.setContentPane(rootPanel);
+        this.setContentPane(jPanel);
         this.pack();
         this.setVisible(true);
     }
