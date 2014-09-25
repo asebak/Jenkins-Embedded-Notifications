ea8f2852ca20a94c9f52feb49e5dee52749d374c
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-25 17:20:37 -0400

    MCU Build Status
    Added Build Status variables to MCU

--- Atmega328Code/JenkinsLED/JenkinsLED.ino
+++ Atmega328Code/JenkinsLED/JenkinsLED.ino
@@ -1,23 +1,25 @@
 /* JenkinsStatus will turn the LED at pin 13 on/off depending
  * on the value serially read.
  */
+const char BUILD_UNSTABLE = '0';
+const char BUILD_BUILDING = '1';
 const char BUILD_SUCCESS = '2';
 const int LED_PIN = 3;
 
 
 void setup() {
   Serial.begin(9600);
-    Serial.println("Hello world");
+  Serial.println("Begin Serial Communication");
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-      int sentBit = Serial.read();
-      if (sentBit == BUILD_SUCCESS) {
-    	  digitalWrite(LED_PIN, LOW);
-      } else{
+      int receivedBit = Serial.read();
+      if (receivedBit == BUILD_SUCCESS) {
     	  digitalWrite(LED_PIN, HIGH);
+      } else{
+    	  digitalWrite(LED_PIN, LOW);
       }
   }
 }
