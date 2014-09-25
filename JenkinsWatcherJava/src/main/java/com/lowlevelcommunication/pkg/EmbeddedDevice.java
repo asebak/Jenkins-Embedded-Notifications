3d5d7c6bc90d125fcf997e1619bc5c1c04795678
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-25 16:10:32 -0400

    Add rxtx fix

--- JenkinsWatcherJava/src/main/java/com/lowlevelcommunication/pkg/EmbeddedDevice.java
+++ JenkinsWatcherJava/src/main/java/com/lowlevelcommunication/pkg/EmbeddedDevice.java
@@ -7,6 +7,12 @@ import java.util.Enumeration;
 /**
  * Created by asebak on 9/22/2014.
  */
+/*
+To fix exception getting serial ports do the following fix
+* put rxtx.dll and rxtxserial.dll into c:/program files/java/jdk1.5.0_02/jre/bin
+put rxtx.jar into c:/program files/java/jdk1.5.0_02/jre/lib/ext
+This is for the HW abstraction on windows.
+*/
 public class EmbeddedDevice {
     public EmbeddedDevice(){
         CommPortIdentifier portId = null;
